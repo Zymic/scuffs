24231ec266e479964038538fd2973f285aa5ed74
Bogieman <tom.b1992@gmail.com>
2012-08-04 14:32:45 +0100

    htaccess and url format

---
+++ home.inc.php
@@ -0,0 +1,90 @@
+<div id="slideshow">
+  <div id="slider1">
+    <div id="slide1">
+      <h1 class="captionHeader">"HEADER TEXT HERE"</h1>
+      <div class="slideCaption">
+        <div>Consider that the whale has nothing that can properly be called a neck; on the contrary, where his head and body seem to join, there, in that very place, is the thickest part of him</div>
+        <p><input class="linkBtn" type="button" value="More Info >" /></p>
+      </div>
+    </div>
+    <div>Slide two content</div>
+    <div>Slide three content</div>
+    <div>And so on...</div>
+  </div>
+</div>
+<div id="body">
+  <div id="bodyWrapper">
+    <h1>Welcome!</h1>
+    <p style="border-bottom: 1px solid #454545;padding-bottom:20px;">
+      Established in 1996 Scuffs and Scratches provide a one stop solution for vehicle repair. Covering the whole of Kent, south London and Sussex, six and a half days a week, either at home or at your work place.
+      We combine traditional body shop techniques with the latest smart repair innovations. This allows us to repair rather than replace a many range of parts, thus passing on the savings to you.
+      We offer a wide range of services tailored for every vehicles individual needs including; alloy wheel refurbishment, body and bumper repairs, dent removal, interior repairs, scratches and glass repairs.
+      For a honest professional service please call us and we will happy to advise and give you an initial quotation. 
+    </p>
+    <div style="float: right">
+      <img src="img/quote_button.png">
+    </div>
+    <h1>Our Services</h1>
+
+    <table id="services">
+      <tr>
+        <td>
+          Paint Work
+        </td>
+        <td>
+          Wheel Repair
+        </td>
+        <td>
+          Scratch Removal
+        </td>
+        <td>
+          Dent Repair
+        </td>
+        <td>
+          Fleet Servicing
+        </td>
+        <td>
+          MOT and Servicing
+        </td>
+      </tr>
+    </table>
+    <table style="width: 100%;margin-top:30px;border-top:1px solid #454545;">
+      <tr>
+        <td style="width: 50%;padding-right:20px;border-right:1px solid #454545;padding-top:20px;">
+          <div style="clear: both;">
+            <div style="width: 180px; height:130px;background-color: white;float: right;">
+            </div>
+            <h1 style="margin-top:0px;padding-top:0px;">Fleet Servicing</h1>
+            <div style="font-size: 13px;">We have the capacity to service Fleets for car rentals and insurance companies including MOT's and bodywork</div>
+          </div>
+          <div style="clear: both;padding-top: 20px;">
+            <div style="width: 180px; height:130px;background-color: white;float: right;">
+            </div>
+            <h1 style="margin-top:0px;padding-top:0px;">Quality Support</h1>
+            <div style="font-size: 13px;">Open 8.00am-5.30pm 5 days a week and 8.00am to 12.30 pm Saturdays our staff are always at hand to deal with your mechanical requirements</div>
+          </div>
+          <div style="clear: both;padding-top: 20px;">
+            <div style="width: 180px; height:130px;background-color: white;float: right;">
+            </div>
+            <h1 style="margin-top:0px;padding-top:0px;">Get in Touch</h1>
+            <div style="font-size: 13px;">Please visit the contact page for full directions to us and contact information</div>
+          </div>
+
+
+        </td>
+        <td style="width: 50%;padding-left:20px;padding-top:10px;">
+          <h1 style="margin-top:0px;padding-top:0px;">
+            Introduction Video
+          </h1>
+          <div style="font-size: 13px;">
+            The building in which i found myself contained the machinery which produces that artifial atmosphere which sustains life on Mars. The secret of the entire process hinges on the use of the ninth ray, one of the beautiful scintillations which I had noted emanating from the great stone in my host's diadem.
+          </div>
+          <div style="width: 100%; height: 250px;background-color: white;margin-top:10px;">
+          </div>
+        </td>
+      </tr>
+    </table>
+
+  </div>
+
+</div>
