f641a7e549f0896f5f856d505595ef1aba9fc82c
Benjamin Littlemore <benjaminlittlemore@yahoo.co.uk>
2012-08-06 15:00:56 +0100

    About Us Changes

--- includes/about.inc.php
+++ includes/about.inc.php
@@ -1,33 +1,65 @@
-<div id="bodyWrapper">
-  <div style="float: right; margin-top: 20px;">
-    <a href="quote"><img src="img/quote_button.png"></a>
-  </div>
-  <h1 style="margin-top: 0px;">Our Services</h1>
-  <table id="services">
-    <tr>
-      <td>
-        Paint Work
-      </td>
-      <td>
-        Wheel Repair
-      </td>
-      <td>
-        Scratch Removal
-      </td>
-      <td>
-        Dent Repair
-      </td>
-      <td>
-        Fleet Servicing
-      </td>
-      <td>
-        MOT and Servicing
-      </td>
-    </tr>
-  </table>
-</div>
-<div id="body">
-  <div id="bodyWrapper">
+<div class="container" style="width: 80%;margin-bottom: 0px;margin: auto;padding-top: 10px;">
     <h1>About us</h1>
+    <p>Full body repair and work completed to a very high standard . Scruffs & Scratches provide an on-time accurate service, with 6 vans covering the whole of Kent, 
+      South London and Sussex, 6 and ½ days a week, either at home or at your workplace. We have highly trained technicians covering any aspect of paint refurbishment 
+      including alloy wheels. For bigger car repair work we have a static workshop which can hold up to 7 cars at any one time and parking facilities for up to 20 cars! 
+      Director Lee Harrison has over 22 years experience in the body shop industry and currently 12 years experience in smart repairs. Scruffs & Scratches covers over a 
+      100 dealerships for main dealer satisfaction and guarantee including Rolls Royce, Maserati and Ferrari.</p>
 
+    <p>Within the workshop we have a Head Paint Technician who is main dealer trained and experienced. Two Panel beaters and a Stripper-Fitter, all main dealer approved. 
+      Our 6 vans are out on the road everyday of the week apart from Sunday offering a very convenient service to many customers whom are unable to get to our workshop. 
+      Our mobile technicians are all body shop trained with previous experience in the body shop field.</p>
+
+    <h2>Trade Bodies / Associations</h2>
+    <p>All staff whether mobile or working statically within the workshop are fully qualified and trained to a very high standard.</p>
+
+    <h2>Specialist Products or Services</h2>
+    <p>Scruffs & Scratches specialise in paint refurbishment on a mobile and static basis. Low environmental painting environment. We have water-based paint systems 
+      available both mobile and within the workshop. Within the workshop we have two low-bake oven facilities.</p>
+
+    <table style="width: 100%;margin-top:30px;border-top:1px solid #454545;">
+      <tr>
+        <td style="width: 20%;border-right:1px solid #454545;padding-top:0px;">
+          <h2>Facilities at our premises</h2>
+          <table id="list" align="center">
+            <tr><td>Customer rest areas</td></tr>
+            <tr><td>Customer toilets</td></tr>
+            <tr><td>Disabled Access</td></tr>
+            <tr><td>Free parking</td></tr>
+            <tr><td>Garden</td></tr>
+          </table>
+        </td>
+
+        <td style="width: 20%;padding-left:20px;border-right:1px solid #454545;padding-top:0px;">
+          <h2>Opening times</h2>
+        </td>
+
+        <td style="width: 20%;padding-left:20px;padding-top:0px;">
+          <h2>What we offer</h2>
+        </td>
+      </tr>
+
+      <tr>
+        <td style="width: 50%;padding-right:20px;border-right:1px solid #454545; border-top:1px solid #454545; padding-top:0px;">
+          <h2>What we offer</h2>
+        </td>
+
+        <td style="width: 50%;padding-left:20px;padding-top:0px; border-top:1px solid #454545;">
+          <h2>Payment Methods Accepted</h2>
+        </td>
+      </tr>
+
+      <tr>
+        <td style="width: 50%;padding-right:20px;border-right:1px solid #454545; border-top:1px solid #454545; padding-top:0px;">
+          <h2>Conveniences Offered</h2>
+        </td>
+
+        <td style="width: 50%;padding-left:20px;padding-top:0px;padding-bottom:20px; border-top:1px solid #454545;">
+          <h2>Coverage Area</h2>
+        </td>
+      </tr>
+    </table>
+
+  <div style="padding-bottom: 10px;">
+  </div>
 </div>
