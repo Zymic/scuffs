59c3e5580fbda60c1c4ddd77a8c83731e2d7d073
Benjamin Littlemore <benjaminlittlemore@yahoo.co.uk>
2012-08-07 15:12:48 +0100

    Google Map Added

--- includes/contact.inc.php
+++ includes/contact.inc.php
@@ -1,4 +1,14 @@
 <div id="bodyWrapper" style="padding-bottom: 10px;font-size: 13px;">
 	<h1>Contact Us</h1>
 
+	<iframe width="900" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+	 src="https://maps.google.co.uk/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Scuffs+%26+Scratches,
+	 +Fishers+Farm+Fishers+Road+Staplehurst+Kent+TN17+2HS&amp;aq=&amp;sll=51.169065,0.558758&amp;sspn=0.017787,
+	 0.043945&amp;ie=UTF8&amp;hq=Scuffs+%26+Scratches,+Fishers+Farm+Fishers+Road+Staplehurst+Kent&amp;hnear=
+	 Sissinghurst+TN17+2HS,+United+Kingdom&amp;t=m&amp;ll=51.1929,0.558243&amp;spn=0.096822,0.30899&amp;z=12&amp;iwloc=A&amp;output=embed">
+	</iframe><br /><small><a href="https://maps.google.co.uk/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Scuffs+%26+Scratches,
+	+Fishers+Farm+Fishers+Road+Staplehurst+Kent+TN17+2HS&amp;aq=&amp;sll=51.169065,0.558758&amp;sspn=0.017787,0.043945&amp;ie=UTF8&amp;
+	hq=Scuffs+%26+Scratches,+Fishers+Farm+Fishers+Road+Staplehurst+Kent&amp;hnear=Sissinghurst+TN17+2HS,+United+Kingdom&amp;t=m&amp;
+	ll=51.1929,0.558243&amp;spn=0.096822,0.30899&amp;z=12&amp;iwloc=A" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+
 </div>
