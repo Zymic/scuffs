ec5ad0876a470b98598bb8b8865ce4fc173e49dd
Bogieman <tom.b1992@gmail.com>
2012-08-07 16:16:20 +0100

    contact page style change

--- includes/contact.inc.php
+++ includes/contact.inc.php
@@ -45,9 +45,7 @@
 		<table>
 			<tr>
 				<td style="width: 10%;">
-					<lable>
 						Name:
-					</label>
 				</td>
 				<td>
 					<input class="contactText" type="text" name="name" />
@@ -55,9 +53,7 @@
 			</tr>
 			<tr>
 				<td>		
-					<label>
 						Email:
-					</label>
 				</td>
 				<td>
 					<input class="contactText" type="email" name="email" />
@@ -65,9 +61,7 @@
 			</tr>
 			<tr>
 				<td style="vertical-align: top;">
-					<label>
 						Message:
-					</label>
 				</td>
 				<td style="vertical-align: top;">
 					<textarea class="contactText" name="message"></textarea>
@@ -81,17 +75,21 @@
 			</tr>
 		</table>
 	</div>
+</div>
 
-
-
-	<iframe width="900" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
-	 src="https://maps.google.co.uk/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Scuffs+%26+Scratches,
-	 +Fishers+Farm+Fishers+Road+Staplehurst+Kent+TN17+2HS&amp;aq=&amp;sll=51.169065,0.558758&amp;sspn=0.017787,
-	 0.043945&amp;ie=UTF8&amp;hq=Scuffs+%26+Scratches,+Fishers+Farm+Fishers+Road+Staplehurst+Kent&amp;hnear=
-	 Sissinghurst+TN17+2HS,+United+Kingdom&amp;t=m&amp;ll=51.1929,0.558243&amp;spn=0.096822,0.30899&amp;z=12&amp;iwloc=A&amp;output=embed">
-	</iframe><br /><small><a href="https://maps.google.co.uk/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Scuffs+%26+Scratches,
-	+Fishers+Farm+Fishers+Road+Staplehurst+Kent+TN17+2HS&amp;aq=&amp;sll=51.169065,0.558758&amp;sspn=0.017787,0.043945&amp;ie=UTF8&amp;
-	hq=Scuffs+%26+Scratches,+Fishers+Farm+Fishers+Road+Staplehurst+Kent&amp;hnear=Sissinghurst+TN17+2HS,+United+Kingdom&amp;t=m&amp;
-	ll=51.1929,0.558243&amp;spn=0.096822,0.30899&amp;z=12&amp;iwloc=A" style="color:#0000FF;text-align:left">View Larger Map</a></small>
-
+<div id="body">
+	<div id="bodyWrapper">
+		<h1>
+			Find Us
+		</h1>
+		<iframe width="900" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+		 src="https://maps.google.co.uk/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Scuffs+%26+Scratches,
+		 +Fishers+Farm+Fishers+Road+Staplehurst+Kent+TN17+2HS&amp;aq=&amp;sll=51.169065,0.558758&amp;sspn=0.017787,
+		 0.043945&amp;ie=UTF8&amp;hq=Scuffs+%26+Scratches,+Fishers+Farm+Fishers+Road+Staplehurst+Kent&amp;hnear=
+		 Sissinghurst+TN17+2HS,+United+Kingdom&amp;t=m&amp;ll=51.1929,0.558243&amp;spn=0.096822,0.30899&amp;z=12&amp;iwloc=A&amp;output=embed">
+		</iframe><br /><small><a href="https://maps.google.co.uk/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Scuffs+%26+Scratches,
+		+Fishers+Farm+Fishers+Road+Staplehurst+Kent+TN17+2HS&amp;aq=&amp;sll=51.169065,0.558758&amp;sspn=0.017787,0.043945&amp;ie=UTF8&amp;
+		hq=Scuffs+%26+Scratches,+Fishers+Farm+Fishers+Road+Staplehurst+Kent&amp;hnear=Sissinghurst+TN17+2HS,+United+Kingdom&amp;t=m&amp;
+		ll=51.1929,0.558243&amp;spn=0.096822,0.30899&amp;z=12&amp;iwloc=A" style="color:white;text-align:left">View Larger Map</a></small>
+	</div>
 </div>
