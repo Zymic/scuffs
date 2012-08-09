a488a86db74caf61bbd5f97dd3516d21e69103d3
Benjamin Littlemore <benjaminlittlemore@yahoo.co.uk>
2012-08-09 11:10:04 +0100

    Gallery Changes

--- includes/gallery.inc.php
+++ includes/gallery.inc.php
@@ -4,18 +4,18 @@
 <div id="folio-wrapper">
 	<table width="850" border="0" cellpadding="0" cellspacing="0" class="table-margin">
         <tr>
-          <td width="268" height="178" align="left" valign="top"><a href="img/gallery/ProMx_Logo.png" rel="lightbox" title="AppearFirst Logo Design" ><img src="img/gallery/thumbs/4.png" alt="IMG" width="268" height="158" /></a></td>
-          <td width="22" height="178" align="left" valign="top">&nbsp;</td>
-          <td width="268" height="178" align="left" valign="top"><a href="img/gallery/ProMx_Logo.png" rel="lightbox" title="WhirlMedia Logo Design - Imposed" ><img src="img/gallery/thumbs/4.png" alt="IMG" width="268" height="158" /></a></td>
-          <td width="22" height="178" align="left" valign="top">&nbsp;</td>
-          <td width="268" height="178" align="left" valign="top"><a href="img/gallery/ProMx_Logo.png" rel="lightbox" title="AppearFirst Website Design" ><img src="img/gallery/thumbs/4.png" alt="IMG" width="268" height="158" /></a></td>
+          <td width="268" height="160" align="left" valign="top"><a href="img/gallery/Scuffs_Logo.png" rel="lightbox" title="Example Text" ><img src="img/gallery/thumbs/scuffs.png" alt="IMG" width="268" height="158" /></a></td>
+          <td width="5" height="160" align="left" valign="top">&nbsp;</td>
+          <td width="268" height="160" align="left" valign="top"><a href="img/gallery/Scuffs_Logo.png" rel="lightbox" title="Example Text" ><img src="img/gallery/thumbs/scuffs.png" alt="IMG" width="268" height="158" /></a></td>
+          <td width="5" height="160" align="left" valign="top">&nbsp;</td>
+          <td width="268" height="160" align="left" valign="top"><a href="img/gallery/Scuffs_Logo.png" rel="lightbox" title="Example Text" ><img src="img/gallery/thumbs/scuffs.png" alt="IMG" width="268" height="158" /></a></td>
         </tr>
         <tr>
-          <td width="268" height="158" align="left" valign="top"><a href="img/gallery/ProMx_Logo.png" rel="lightbox" title="ProMx Logo Design" ><img src="img/gallery/thumbs/4.png" alt="IMG" width="268" height="158" /></a></td>
-          <td width="22" height="158" align="left" valign="top">&nbsp;</td>
-          <td width="268" height="158" align="left" valign="top"><a href="img/gallery/ProMx_Logo.png" rel="lightbox" title="TheAdvertWall - Imposed" ><img src="img/gallery/thumbs/4.png" alt="IMG" width="268" height="158" /></a></td>
-          <td width="22" height="158" align="left" valign="top">&nbsp;</td>
-          <td width="268" height="158" align="left" valign="top"><a href="img/gallery/ProMx_Logo.png" rel="lightbox" title="London 2012 Poster" ><img src="img/gallery/thumbs/4.png" alt="IMG" width="268" height="158" /></a></td>
+          <td width="268" height="160" align="left" valign="top"><a href="img/gallery/Scuffs_Logo.png" rel="lightbox" title="Example Text" ><img src="img/gallery/thumbs/scuffs.png" alt="IMG" width="268" height="158" /></a></td>
+          <td width="5" height="160" align="left" valign="top">&nbsp;</td>
+          <td width="268" height="160" align="left" valign="top"><a href="img/gallery/Scuffs_Logo.png" rel="lightbox" title="Example Text" ><img src="img/gallery/thumbs/scuffs.png" alt="IMG" width="268" height="158" /></a></td>
+          <td width="5" height="160" align="left" valign="top">&nbsp;</td>
+          <td width="268" height="160" align="left" valign="top"><a href="img/gallery/Scuffs_Logo.png" rel="lightbox" title="Example Text" ><img src="img/gallery/thumbs/scuffs.png" alt="IMG" width="268" height="158" /></a></td>
         </tr>
     </table>      
   </div>
